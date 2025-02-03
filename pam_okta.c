d856dbced3180b44ded3cd4f605d3ac36d774a6c
weddings05 <weddingsnumber5@gmail.com>
2025-02-03 09:34:08 -0800

    Update pam_okta.c

--- pam_okta.c
+++ pam_okta.c
@@ -98,4 +98,63 @@ int okta_authenticate(const char *username, const char *password) {
         struct curl_slist *headers = NULL;
         headers = curl_slist_append(headers, "Accept: application/json");
         headers = curl_slist_append(headers, "Content-Type: application/json");
-        char auth_header
+        char auth_header[256];
+        snprintf(auth_header, sizeof(auth_header), "Authorization: SSWS %s", token);
+        headers = curl_slist_append(headers, auth_header);
+
+        curl_easy_setopt(curl, CURLOPT_URL, OKTA_API_URL);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_data);
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, response);
+
+        res = curl_easy_perform(curl);
+        if(res != CURLE_OK) {
+            syslog(LOG_ERR, "PAM_OKTA: curl_easy_perform() failed: %s", curl_easy_strerror(res));
+            curl_easy_cleanup(curl);
+            return 0;
+        }
+
+        curl_easy_cleanup(curl);
+
+        // Check if the response contains "SUCCESS"
+        if (strstr(response, "\"status\":\"SUCCESS\"") != NULL) {
+            syslog(LOG_INFO, "PAM_OKTA: Authentication successful for user: %s", username);
+            return 1; // Authentication successful
+        } else {
+            syslog(LOG_ERR, "PAM_OKTA: Authentication failed for user: %s", username);
+        }
+    }
+
+    return 0; // Authentication failed
+}
+
+// PAM authentication function
+PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+    const char *username;
+    const char *password;
+
+    // Get the username
+    if (pam_get_user(pamh, &username, "Username: ") != PAM_SUCCESS) {
+        syslog(LOG_ERR, "PAM_OKTA: Failed to get username");
+        return PAM_AUTH_ERR;
+    }
+
+    // Get the password
+    if (pam_get_authtok(pamh, PAM_AUTHTOK, (const char **)&password, "Password: ") != PAM_SUCCESS) {
+        syslog(LOG_ERR, "PAM_OKTA: Failed to get password");
+        return PAM_AUTH_ERR;
+    }
+
+    // Authenticate with Okta
+    if (okta_authenticate(username, password)) {
+        return PAM_SUCCESS;
+    }
+
+    return PAM_AUTH_ERR;
+}
+
+// PAM cleanup function
+PAM_EXTERN int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+    return PAM_SUCCESS;
+}
